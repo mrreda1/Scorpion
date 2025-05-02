3a42408735efbc0f60ace14e31bd536b57c96d5d
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2025-05-03 01:37:08 +0300

    Fix testing files after update data types

--- testing/main.cpp
+++ testing/main.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 int main() {
-    vector<Token *> tokens;
+    vector<Token> tokens;
 
-    T_SInteger *sint = new T_SInteger(54, negative);
-    T_Character *charac = new T_Character('a');
+    T_SInteger sint = new T_SInteger_(54, negative);
+    T_Character charac = new T_Character_('a');
 
     tokens.push_back(sint);
     tokens.push_back(charac);
 
-    for (Token *token : tokens) {
+    for (Token token : tokens) {
         if (token->getTokenType() == Variable) {
-            T_var *var = dynamic_cast<T_var *>(token);
+            T_var var = dynamic_cast<T_var>(token);
             cout << "ID: " << var->getID() << endl;
             if (var->getDataType() == SInteger) {
-                T_SInteger *sinteger = dynamic_cast<T_SInteger *>(var);
+                T_SInteger sinteger = dynamic_cast<T_SInteger>(var);
                 cout << "Type: signed integer\n";
                 cout << "Value: " << sinteger->getValue() << endl;
                 cout << "Sign: "
                      << (sinteger->getSign() == positive ? '+' : '-') << endl;
                 cout << endl;
             } else if (var->getDataType() == Character) {
-                T_Character *character = dynamic_cast<T_Character *>(var);
+                T_Character character = dynamic_cast<T_Character>(var);
                 cout << "Type: character\n";
                 cout << "Chracter: " << character->getValue() << endl;
                 cout << endl;
