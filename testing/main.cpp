d663fe90d3f1a90974949957fa109b2014b71a36
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2025-05-03 05:33:17 +0300

    testing update

--- testing/main.cpp
+++ testing/main.cpp
@@ -4,31 +4,32 @@
 using namespace std;
 
 int main() {
-    vector<Token> tokens;
+    vector<T_var> variables;
+    T_Character var_c = new T_Character_("c", 'a');
+    T_Integer var_x = new T_Integer_("x", 4543);
+    T_Integer var_y = new T_Integer_("y");
+    Token token = new Token_(Condition);
 
-    T_SInteger sint = new T_SInteger_(54, negative);
-    T_Character charac = new T_Character_('a');
+    variables.push_back(var_c);
+    variables.push_back(var_x);
+    variables.push_back(var_y);
 
-    tokens.push_back(sint);
-    tokens.push_back(charac);
-
-    for (Token token : tokens) {
-        if (token->getTokenType() == Variable) {
-            T_var var = dynamic_cast<T_var>(token);
-            cout << "ID: " << var->getID() << endl;
-            if (var->getDataType() == SInteger) {
-                T_SInteger sinteger = dynamic_cast<T_SInteger>(var);
-                cout << "Type: signed integer\n";
-                cout << "Value: " << sinteger->getValue() << endl;
-                cout << "Sign: "
-                     << (sinteger->getSign() == positive ? '+' : '-') << endl;
-                cout << endl;
-            } else if (var->getDataType() == Character) {
-                T_Character character = dynamic_cast<T_Character>(var);
-                cout << "Type: character\n";
-                cout << "Chracter: " << character->getValue() << endl;
-                cout << endl;
-            }
+    for (T_var var : variables) {
+        cout << "\nID: " << var->getID() << '\n';
+        switch (var->getDataType()) {
+            case D_Character: {
+                T_Character char_var = dynamic_cast<T_Character>(var);
+                cout << "Type: Character\n";
+                cout << "Char: " << char_var->getValue() << '\n';
+            } break;
+            case D_Integer: {
+                T_Integer int_var = dynamic_cast<T_Integer>(var);
+                cout << "Type: Integer\n";
+                cout << "Value: " << int_var->getValue() << '\n';
+            } break;
         }
     }
+
+    cout << T_var_::getVariable(0)->getName() << '\n';
+    cout << T_var_::getVariable("y")->getID() << '\n';
 }
